c8653565edd3c238354a66b129b7f414fb159286
FarCodeFR <Timo-44@hotmail.com>
2025-05-06 18:34:05 +0200

    Suppression du try catch dans le fichier controlleur

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,69 +1,56 @@
 import {
-	Controller,
-	Get,
-	Post,
-	HttpCode,
-	HttpStatus,
-	Body,
-	Delete,
-	Put,
-	Param,
-	BadRequestException,
-} from "@nestjs/common";
-import { AppService } from "./app.service";
-import { CreateCardDto } from "./validation/create-card.dto";
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Body,
+  Delete,
+  Put,
+  Param,
+  BadRequestException,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { CreateCardDto } from './validation/create-card.dto';
 
-@Controller("cards")
+@Controller('cards')
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-	@Get()
-	async readAllCards() {
-		try {
-			return await this.appService.readAll();
-		} catch (err) {
-			console.error(err);
-		}
-	}
+  @Get()
+  async readAllCards() {
+    return await this.appService.readAll();
+  }
 
-	@Post()
-	async create(@Body() createCardDto: CreateCardDto){
-		try{
-			const newCard = await this.appService.create(createCardDto);
-			return newCard;
-		}catch(err){
-			console.error(err)
-		}
-	}
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createCardDto: CreateCardDto) {
+    const newCard = await this.appService.create(createCardDto);
+    return newCard;
+  }
 
-	@Delete(":id")
-	@HttpCode(HttpStatus.NO_CONTENT)
-	async deleteCard(@Param('id') id: string) {
-		try {
-			console.log("Tentative suppression carte avec id:", id);
-			const response = await this.appService.delete(Number(id));
-			return response;
-		} catch (err) {
-			console.error(err);
-		}
-	}
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteCard(@Param('id') id: string) {
+    const response = await this.appService.delete(Number(id));
+    return response;
+  }
 
-
-
-	@Put(":id")
-	async updateCompleted(
-		@Param("id") id: string, 
-		@Body() body: {completed: boolean, title:string})
-		{
-			const cardId = parseInt(id, 10);
-			if (isNaN(cardId)) 
-				throw new BadRequestException("ID invalide");
-			if (body.title !== undefined) {
-				return await this.appService.updateTitle(cardId, { title: body.title });
-			}
-			if (body.completed !== undefined) {
-				return await this.appService.updateChecked(cardId, { completed: body.completed });
-			}
-			throw new BadRequestException("Aucune donnée à mettre à jour");
-		}
+  @Put(':id')
+  async updateCompleted(
+    @Param('id') id: string,
+    @Body() body: { completed: boolean; title: string },
+  ) {
+    const cardId = parseInt(id, 10);
+    if (isNaN(cardId)) throw new BadRequestException('ID invalide');
+    if (body.title !== undefined) {
+      return await this.appService.updateTitle(cardId, { title: body.title });
+    }
+    if (body.completed !== undefined) {
+      return await this.appService.updateChecked(cardId, {
+        completed: body.completed,
+      });
+    }
+    throw new BadRequestException('Aucune donnée à mettre à jour');
+  }
 }
--- apps/api/src/validation/create-card.dto.ts
+++ apps/api/src/validation/create-card.dto.ts
@@ -1,16 +1,24 @@
-import { IsString, IsNotEmpty, IsBoolean, Length, IsOptional } from "class-validator";
+import {
+  IsString,
+  IsNotEmpty,
+  IsBoolean,
+  Length,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateCardDto {
   @IsString()
-  @IsNotEmpty({ message: "Le titre ne peut pas être vide" })
-  @Length(5, 20, {message: "La longeur du text doit être entre 5 et 20 caractères"})
+  @IsNotEmpty({ message: 'Le titre ne peut pas être vide' })
+  @Length(5, 20, {
+    message: 'La longeur du text doit être entre 5 et 20 caractères',
+  })
   title: string;
-  
+
   @IsBoolean()
   @IsOptional()
   completed?: boolean;
 }
 
 export class UpdateCardDto {
-	@IsBoolean() completed: boolean;
+  @IsBoolean() completed: boolean;
 }
